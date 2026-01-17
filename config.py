99b1ce9d48cad33fc9e388be483697d8136e1326
itsprince05 <102887695+itsprince05@users.noreply.github.com>
2026-01-17 12:47:22 +0530

    Updated config.py

--- config.py
+++ config.py
@@ -8,7 +8,9 @@ API_ID = os.getenv("API_ID", "38659771")
 API_HASH = os.getenv("API_HASH", "6178147a40a23ade99f8b3a45f00e436")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7872058354:AAHz2ney8VpIKNLl-k2cA7y5nbQCKlDf4bM")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "6701020399"))
-LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "-1003609401157"))
+UPDATE_GROUP_ID = -1003609401157
+LOG_GROUP_NORMAL = -1003674205198
+LOG_GROUP_TIMER = -1003592103153
 
 # Paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
